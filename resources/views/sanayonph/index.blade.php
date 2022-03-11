4ba3d4581bf9ba4bc325c3b4a918705603e25fd2
kulotsystems <kulotsystems@gmail.com>
2022-03-11 21:42:46 +0800

    Integrated Messenger Chat Plugin.

--- resources/views/sanayonph/index.blade.php
+++ resources/views/sanayonph/index.blade.php
@@ -3,11 +3,13 @@
 <head>
     <title>{{ env('APP_NAME') }}</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link href="{{ asset('images/favicon.png') }}" rel="icon" type="image/vnd.microsoft.icon" />
 </head>
 <body>
     <div id="main"></div>
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
     <script src="{{ mix('js/vendor/manifest.js') }}"></script>
     <script src="{{ mix('js/vendor/packages/vue.js') }}"></script>
     <script src="{{ mix('js/vendor/packages/roboto.js') }}"></script>
